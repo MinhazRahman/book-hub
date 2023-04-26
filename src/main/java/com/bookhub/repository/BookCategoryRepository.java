f958cee187ddb6a7c0c68ef264caf9c89a88cf6f
Mohammed Rahman <minhaz357@gmail.com>
2023-04-26 10:36:09 -0400

    Add @Repository annotation

--- src/main/java/com/bookhub/repository/BookCategoryRepository.java
+++ src/main/java/com/bookhub/repository/BookCategoryRepository.java
@@ -2,6 +2,8 @@ package com.bookhub.repository;
 
 import com.bookhub.model.BookCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
 }
--- src/main/java/com/bookhub/repository/BookRepository.java
+++ src/main/java/com/bookhub/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.bookhub.repository;
 
 import com.bookhub.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
