6e079ff89ef1de80f47b59dc17b7bd02163bdf5e
Mohammed Rahman <minhaz357@gmail.com>
2023-05-02 17:09:01 -0400

    Create model class Address
    	- Model class Address maps to the table 'address'

---
+++ src/main/java/com/bookhub/model/Address.java
@@ -0,0 +1,33 @@
+package com.bookhub.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+}
