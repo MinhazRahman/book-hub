366d34c0f015e4ed10d3ae14e5556eb70305aa62
Mohammed Rahman <minhaz357@gmail.com>
2023-06-03 23:20:23 -0400

    Update CheckoutService to create a PaymentIntent

--- src/main/java/com/bookhub/service/CheckoutService.java
+++ src/main/java/com/bookhub/service/CheckoutService.java
@@ -1,8 +1,12 @@
 package com.bookhub.service;
 
+import com.bookhub.dto.PaymentInfo;
 import com.bookhub.dto.Purchase;
 import com.bookhub.dto.PurchaseResponse;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 public interface CheckoutService {
     PurchaseResponse placeOrder(Purchase purchase);
+    PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException;
 }
