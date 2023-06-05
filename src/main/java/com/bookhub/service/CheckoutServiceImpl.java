45fd44fc9fe45cb89f391d6669225a0d04481956
Mohammed Rahman <minhaz357@gmail.com>
2023-06-04 21:27:19 -0400

    Add description to payment info

--- src/main/java/com/bookhub/service/CheckoutServiceImpl.java
+++ src/main/java/com/bookhub/service/CheckoutServiceImpl.java
@@ -77,6 +77,7 @@ public class CheckoutServiceImpl implements CheckoutService{
         params.put("amount", paymentInfo.getAmount());
         params.put("currency", paymentInfo.getCurrency());
         params.put("payment_method_types", paymentMethodTypes);
+        params.put("description", "BookHub payment");
 
         return PaymentIntent.create(params);
     }
